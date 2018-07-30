02bc31287b4fbdc3e29a424f443b121d628af25c
Bilal Ahmad <pro123908@gmail.com>
2018-07-30 20:35:54 +0500

    test

--- test.php
+++ test.php
@@ -1,1 +1,12 @@
 
+
+<?php
+if (isset($_POST['login'])) {
+    echo "BILL";
+}
+?>
+
+<form action="test.php" method='POST'>
+    <input type="text" name='name'>
+    <button type='submit' name='login'>Done</button>
+</form>
